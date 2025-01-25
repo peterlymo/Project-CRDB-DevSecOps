d9e6440e4682d2a7ef3c7aabade342218d2dbd34
lymo <30964644+peterlymo@users.noreply.github.com>
2025-01-25 15:22:39 +0300

    Update NumericController.java- fix useless local variable assignemnt

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -21,16 +21,10 @@ public class NumericController {
         return "Kubernetes DevSecOps";
     }
 
-    @GetMapping("/compare/{value}")
-    public String compareToFifty(@PathVariable int value) {
-        String message = "Could not determine comparison";
-        if (value > 50) {
-            message = "Greater than 50";
-        } else {
-            message = "Smaller than or equal to 50";
-        }
-        return message;
-    }
+@GetMapping("/compare/{value}")
+public String compareToFifty(@PathVariable int value) {
+    return value > 50 ? "Greater than 50" : "Smaller than or equal to 50";
+}
 
     @GetMapping("/increment/{value}")
     public int increment(@PathVariable int value) {
