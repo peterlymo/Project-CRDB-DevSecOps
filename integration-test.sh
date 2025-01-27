ebf08e580f74de1cc75ac44844680cb44ac569df
lymo <30964644+peterlymo@users.noreply.github.com>
2025-01-27 10:13:40 +0300

    Create integration-test.sh

---
+++ integration-test.sh
@@ -0,0 +1,24 @@
+sleep 5s
+
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+
+echo $PORT
+echo $applicationURL:${PORT}
+
+if [[ ! -z "$PORT" ]];
+then
+
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT)
+
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP Status Code Test Passed"
+        else
+            echo "HTTP Status code is not 200"
+            exit 1;
+    fi;
+
+else
+        echo "The Service does not have a NodePort"
+        exit 1;
+fi;
