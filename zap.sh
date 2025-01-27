d8012abcc3d0a20a3d6455c85bbcedd84b204c3d
lymo <30964644+peterlymo@users.noreply.github.com>
2025-01-27 13:45:32 +0300

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -12,3 +12,19 @@ docker run -v $(pwd):/zap/wrk/:rw -t quay.io/anshuk6469/owaspzap-report zap-api-
 #docker run -v $(pwd)/zap-report:/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 
 exit_code=$?
+
+
+
+# HTML Report
+ sudo mkdir -p owasp-zap-report
+ sudo mv zap_report.html owasp-zap-report
+
+
+echo "Exit Code : $exit_code"
+
+ if [[ ${exit_code} -ne 0 ]];  then
+    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+    exit 1;
+   else
+    echo "OWASP ZAP did not report any Risk"
+ fi;
